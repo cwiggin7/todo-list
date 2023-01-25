ff51d4fb6f412b848d78ac8a28c48fcd01c251dc
cwiggin7 <cwiggin7@ksu.edu>
2023-01-25 17:51:48 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const todos = (title, description, dueDate, priority, projectID) => {
+    const ID = crypto.randomUUID();
+    const changePriority = (number) => {
+        priority = number;
+        localStorage.setItem(this.ID, JSON.stringify(this));
+    };
+    return { title, ID, description, dueDate, priority, projectID };
+};
+
+const projects = (title) => {
+    const ID = crypto.randomUUID();
+    return { title, ID };
+};
+
+function createNewProject(title) {
+    var proj = projects(title);
+    localStorage.setItem(proj.ID, JSON.stringify(proj));
+}
+
+function removeProject(ID) {
+    localStorage.removeItem(ID);
+}
+
+function createNewTodo(title, description, dueDate, priority, projectID) {
+    var todo = todos(title, description, dueDate, priority, projectID);
+    localStorage.setItem(todo.ID, JSON.stringify(todo));
+}
+
+(function () {
+    console.log("this is a test.");
+})();
